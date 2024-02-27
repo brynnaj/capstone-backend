688e28db28e3bf812a31807a0c27fdbd571541ce
clerickbarrion <clerickbarrion@gmail.com>
2024-02-27 16:16:32 -0500

    added chat history

--- app.js
+++ app.js
@@ -11,15 +11,16 @@ app.use(express.json());
 
 app.post('/api/botMessage', async (req, res) => {
     const { message } = req.body;
+    const { history } = req.body;
     const completion = await openai.chat.completions.create({
           messages: [
             {
               role: "system",
               // Will feed the AI our html code later so that it knows the functionality of our website
               content: `You are a chatbot named Kale for a loan management website. 
-                        You are to answer queries that the user has. Your response must not be longer than 1 paragraph.`,
+                        You are to answer queries that the user has. Your response must not be longer than 1 paragraph.
+                        Here is the conversation so far ||| ${history} |||`,
             },
-            { role: "assistant", content: 'Hi there! What can I help you with today?' },
             { role: "user", content: message },
           ],
           model: "gpt-3.5-turbo-16k-0613",
